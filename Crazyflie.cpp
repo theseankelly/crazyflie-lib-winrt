8c0c0a094a2ef713cc0d0800deb03301858b1270
Sean Kelly <theseankelly@outlook.com>
2018-11-04 07:15:40 -0500

    Renaming project files from defaults

---
+++ Crazyflie.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "Crazyflie.h"
+
+namespace winrt::bitcraze::crazyflielib::implementation
+{
+    int32_t Crazyflie::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void Crazyflie::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ Crazyflie.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Crazyflie.g.h"
+
+namespace winrt::bitcraze::crazyflielib::implementation
+{
+    struct Crazyflie : CrazyflieT<Crazyflie>
+    {
+        Crazyflie() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::bitcraze::crazyflielib::factory_implementation
+{
+    struct Crazyflie : CrazyflieT<Crazyflie, implementation::Crazyflie>
+    {
+    };
+}
